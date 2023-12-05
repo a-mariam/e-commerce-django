7307ac7f95c70a0e94a4a1566147ea2bf9f12ea6
a-mariam <mariiam22222@gmail.com>
2023-12-05 11:05:19 +0100

    done with the class based view

--- store/urls.py
+++ store/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 
 from store import views
+#   function base
+# urlpatterns = [
+#     path('products/', views.product_list),
+#     path('products/<int:pk>/', views.product_details)
+# ]
 
-urlPatterns = [
-    path('products/', views.product_list),
-    path('products/<int:pk>/', views.product_details)
+
+urlpatterns = [
+    path('products/', views.ProductList.as_view()),
+    path('products/<int:pk>/', views.ProductDetails.as_view())
 ]
--- store/views.py
+++ store/views.py
@@ -2,28 +2,70 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from django.http import HttpRequest
 from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from .serializers import  ProductSerializer
 from django.shortcuts import get_object_or_404
 from rest_framework import status
 from .models import Product
+
 # Create your views here.
+# function base
+# @api_view(['GET', 'POST'])
+# def product_list(request):
+#     if request.method == 'GET':
+#         products = Product.objects.all()
+#         serializer = ProductSerializer(products, many=True)
+#         return Response(serializer.data)
+#     elif request.method == 'POST':
+#         serializer = ProductSerializer(data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response(status=status.HTTP_201_CREATED, data=serializer.data)
+#
+#
+# @api_view(['GET', 'POST', 'PATCH', 'DELETE'])
+# def product_details(resquest, pk):
+#     product = get_object_or_404(Product, pk=pk)
+#     if resquest.method == 'GET':
+#         serializer = ProductSerializer(product)
+#         return Response(status=status.HTTP_200_OK, data=serializer.data)
+#     elif resquest.method == 'PUT':
+#         serializer = ProductSerializer(product,data=resquest.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response(status=status.HTTP_200_OK, data=serializer)
+#     elif resquest.method == 'DELETE':
+#         product.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-@api_view(['GET', 'POST'])
-def product_list(request):
-    if request.method == 'GET':
+# Using oop concept which
+class ProductList(APIView):
+    def get(self, request):
         products = Product.objects.all()
         serializer = ProductSerializer(products, many=True)
-        return Response(serializer.data)
-    elif request.method == 'POST':
+        return Response(status=status.HTTP_200_OK,data=serializer.data)
+
+    def post(self, request):
         serializer = ProductSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        return Response(status=status.HTTP_201_CREATED, data=serializer.data )
+        return Response(status=status.HTTP_201_CREATED, data= serializer.data)
 
 
-@api_view()
-def product_details(resquest, pk):
-    product = get_object_or_404(Product, pk=pk)
-    serializer = ProductSerializer(product)
-    return HttpRequest(serializer.data)
+class ProductDetails(APIView):
+    def get(self, request, pk):
+        product = get_object_or_404(Product, pk=pk)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        product = get_object_or_404(Product, pk=pk)
+        serializer = ProductSerializer(product, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+    def delete(self, request, pk):
+        product = get_object_or_404(Product, pk=pk)
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
